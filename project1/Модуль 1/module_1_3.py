9d763268f3135eb1ef3d3cbe5f7d88f408615fb5
Vladimir Ni12ck <ni12ck@mail.ru>
2024-10-09 16:03:57 +0300

    Первая лекция

---
+++ project1/Модуль 1/module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Vladimir'
+print('Имя:', name)
+age = 29
+print('Возраст:', age)
+age = 29+1
+print('Новый возраст:', age)
+is_student = True
+print('Студент:', is_student)
