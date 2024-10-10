e22946e7a262a217f8fd5a9fee14f62f8636f731
Vladimir Ni12ck <ni12ck@mail.ru>
2024-10-10 16:50:49 +0300

    Практическое задание по теме "Переменные"

--- project1/Модуль 1/Практическое задание по теме Переменные.py
+++ project1/Модуль 1/Практическое задание по теме Переменные.py
@@ -15,3 +15,5 @@ print('Время на одно задание:', Time_spent_on_one_task)
 #Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
 print('Курс: ', Course_name,', всего задач: ', Number_of_homework_completed , ', затрачено часов: ', Number_of_hours_spent, ', среднее время выполнения ', Time_spent_on_one_task, ' часа.', sep='')
 
+# Ещё метод вывода, так для себя
+print(f'Курс: {Course_name}, всего задач: {Number_of_homework_completed}, затрачено часов: {Number_of_hours_spent}, среднее время выполнения {Time_spent_on_one_task} часа.')
