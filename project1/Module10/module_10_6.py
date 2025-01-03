2eacd56858af43516f5fe8a215fbcbf33b43c42b
Vladimir Ni12ck <ni12ck@mail.ru>
2025-01-03 07:05:09 +0300

    Домашнее задание по теме "Очереди для обмена данными между потоками."

--- project1/Module10/module_10_6.py
+++ project1/Module10/module_10_6.py
@@ -1,7 +1,7 @@
 # Задача "Потоки гостей в кафе":
 # Необходимо имитировать ситуацию с посещением гостями кафе.
 # Создайте 3 класса: Table, Guest и Cafe.
-
+import queue
 # I
 # Класс Table:
 # 1. Объекты этого класса должны создаваться следующим способом - Table(1)
@@ -66,7 +66,7 @@ class Table:
 class Guest(threading.Thread):
     # Атрибуты объекта класса
     def __init__(self, name):
-        super().__init__(name)
+        super().__init__()  # вызов конструктора родительского класса
         self.name = name  # имя гостя
 
     # Метод run, где происходит ожидание случайным образом от 3 до 10 секунд
@@ -83,21 +83,101 @@ class Cafe:
 
     # Создал метод guest_arrival (прибытие гостей)
     def guest_arrival(self, *guests):
-        # 1. Должен принимать неограниченное кол-во гостей (объектов класса Guest).
-        # 2. Далее, если есть свободный стол, то сажать гостя за стол (назначать столу guest), запускать поток гостя и выводить
-        # на экран строку "<имя гостя> сел(-а) за стол номер <номер стола>".
-        # 3. Если же свободных столов для посадки не осталось, то помещать гостя в очередь queue и выводить сообщение
-        # "<имя гостя> в очереди".
+        for guest in guests:
+            for table in self.tables:
+                # Если есть свободный стол, то сажаем гостя за стол (назначаем столу guest)
+                if table.guest is None:
+                    table.guest = guest
+                    # Запуск потока гостя
+                    guest.start()
+                    # Вывод строки "<имя гостя> сел(-а) за стол номер <номер стола>"
+                    print(f'{guest.name} сел(-а) за стол номер {table.number}')
+                    break
+            else:
+                # Если же свободных столов для посадки не осталось, то помещать гостя в очередь queue
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
 
-
+    # Первый вариант метода discuss_guests
     # Создал метод discuss_guests (обслужить гостей)
     def discuss_guests(self):
-        # 1. Обслуживание должно происходить пока очередь не пустая (метод empty) или хотя бы один стол занят.
-        # 2. Если за столом есть гость(поток) и гость(поток) закончил приём пищи(поток завершил работу - метод is_alive), то
-        # вывести строки "<имя гостя за текущим столом> покушал(-а) и ушёл(ушла)" и "Стол номер <номер стола> свободен". Так же
-        # текущий стол освобождается (table.guest = None).
-        # 3. Если очередь ещё не пуста (метод empty) и стол один из столов освободился (None), то текущему столу присваивается
-        # гость взятый из очереди (queue.get()). Далее выводится строка "<имя гостя из очереди> вышел(-ла) из очереди и сел(-а)
-        # за стол номер <номер стола>"
-        # 4. Далее запустить поток этого гостя (start)
-        pass
+        while not self.queue.empty() or self.search_table():
+            for table in self.tables:
+                # Если за столом есть гость(поток) и гость(поток) закончил приём пищи(поток завершил работу - метод
+                # is_alive)
+                if table.guest is not None and not table.guest.is_alive():
+                    # Вывод строки "<имя гостя за текущим столом> покушал(-а)
+                    print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
+                    # Вывод строки "Стол номер <номер стола> свободен"
+                    print(f'Стол номер {table.number} свободен')
+                    # Текущий стол освобождается (table.guest = None)
+                    table.guest = None
+                    # Если очередь ещё не пуста (метод empty) и один из столов освободился (None)
+                    if not self.queue.empty():
+                        # текущему столу присваивается гость взятый из очереди (queue.get())
+                        table.guest = self.queue.get()
+                        # Вывод строки "<имя гостя из очереди> вышел(-ла) из очереди и сел(-а) за стол номер <номер
+                        # стола>"
+                        print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                        # Запуск потока этого гостя (start)
+                        table.guest.start()
+
+    # Метод поиска свободного стола
+    def search_table(self):
+        for table in self.tables:
+            if table.guest is not None:
+                return True
+        return False
+
+    # Второй вариант метода discuss_guests
+    # Создал метод discuss_guests (обслужить гостей)
+    def discuss_guests_recursive(self):
+        is_guests_active = False
+        for table in self.tables:
+            # Если за столом есть гость(поток) и гость(поток) закончил приём пищи(поток завершил работу - метод
+            # is_alive)
+            if table.guest is not None and not table.guest.is_alive():
+                # Вывод строки "<имя гостя за текущим столом> покушал(-а)
+                print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
+                # Вывод строки "Стол номер <номер стола> свободен"
+                print(f'Стол номер {table.number} свободен')
+                # Текущий стол освобождается (table.guest = None)
+                table.guest = None
+                # Если очередь ещё не пуста (метод empty) и один из столов освободился (None)
+                if not self.queue.empty():
+                    # текущему столу присваивается гость взятый из очереди (queue.get())
+                    table.guest = self.queue.get()
+                    # Вывод строки "<имя гостя из очереди> вышел(-ла) из очереди и сел(-а) за стол номер <номер
+                    # стола>"
+                    print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                    # Запуск потока этого гостя (start)
+                    table.guest.start()
+            if table.guest is not None:
+                is_guests_active = True
+        if is_guests_active or not self.queue.empty():
+            # Ожидание, чтобы не достигнуть максимального лимита рекурсии
+            time.sleep(0.3)
+            self.discuss_guests_recursive()
+
+
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+
+# Имена гостей
+guests_names = ['Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman', 'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya',
+                'Alexandra']
+
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+
+# Приём гостей
+cafe.guest_arrival(*guests)
+
+# Обслуживание гостей
+# Первый вариант метода discuss_guests
+# cafe.discuss_guests()
+# Второй вариант метода discuss_guests
+cafe.discuss_guests_recursive()
