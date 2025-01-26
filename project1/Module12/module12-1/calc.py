aef792a3d14f0f35d5f2c0ba8818782e35e59a98
Vladimir Ni12ck <ni12ck@mail.ru>
2025-01-26 16:53:11 +0300

    Лекция. Логирование

--- project1/Module12/module12-1/calc.py
+++ project1/Module12/module12-1/calc.py
@@ -1,3 +1,6 @@
+# библиотека для логирования
+import logging
+
 def add(a, b):
     return a + b
 
@@ -11,7 +14,13 @@ def mul(a, b):
 
 
 def div(a, b):
-    return a / b
+    try:
+        a / b
+        logging.info(f'Delenie {a} na {b}')
+        return a / b
+    except ZeroDivisionError as err:
+        logging.error('Delenie na nol', exc_info=True)
+        return 0
 
 def sqrt(a):
     return a**0.5
@@ -20,4 +29,16 @@ def pow(a,b):
     return a**b
 
 if __name__ == '__main__':
-    print(add(100, 3))
+    # print(add(100, 3))
+    # Запись в файл
+    logging.basicConfig(level=logging.INFO, filemode='w',filename='logs.log',
+                        format='%(asctime)s | %(levelname)s | %(message)s' )
+    # logging.debug('Сообщения уровня') # Сообщения уровня
+    # logging.info('Информационные сообщения') #  Информационные сообщения
+    # logging.warning('Сообщения уровня предупреждений') # Сообщения уровня предупреждений
+    # logging.error('Сообщения уровня ошибок') # Сообщения уровня ошибок
+    # # Сообщения уровня критических ошибок, указывающих на серьезные проблемы
+    # logging.critical('Сообщения уровня критических ошибок, указывающих на серьезные проблемы')
+
+print(div(3,4))
+print(div(3,0))
